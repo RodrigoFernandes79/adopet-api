d255747a29affb9f25f68f4a70202d35eeb5dd84
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-04-15 17:18:02 -0300

    projeto inicial

---
+++ src/main/java/com/adopet/apiadopet/ApiAdopetApplication.java
@@ -0,0 +1,13 @@
+package com.adopet.apiadopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdopetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adopet/apiadopet/ApiAdopetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adopet.apiadopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
